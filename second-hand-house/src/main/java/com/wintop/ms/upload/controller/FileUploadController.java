47525ac3672ca474f00c1c23825f38ee69167e4e
Luk123 <13619585427@163.com>
2018-03-04 15:51:39 +0800

    Merge branch 'master' of https://github.com/Luk123/sencondHandHouse

--- second-hand-house/src/main/java/com/wintop/ms/upload/controller/FileUploadController.java
+++ second-hand-house/src/main/java/com/wintop/ms/upload/controller/FileUploadController.java
@@ -1,5 +1,8 @@
 package com.wintop.ms.upload.controller;
 
+import com.wintop.ms.common.base.ServiceResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +13,8 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -19,7 +23,12 @@ import java.util.List;
  */
 @Controller
 public class FileUploadController {
-
+    /** 最大文件大小 */
+    private static long                    maxSize = 10 * 1024 * 1024;
+    private static Logger logger  = LoggerFactory.getLogger(FileUploadController.class);
+    private static final String            IMAGE   = "image";
+    /** 定义允许上传的文件扩展名 */
+    private static HashMap<String, String> extMap  = new HashMap<String, String>();
     /*
      * 获取file.html页面
      */
@@ -27,30 +36,46 @@ public class FileUploadController {
     public String file(){
         return "/file";
     }
-
+    /*
+     * 获取multifile.html页面
+     */
+    @RequestMapping("multifile")
+    public String multifile(){
+        return "/multifile";
+    }
     /**
      * 实现多文件上传
      * */
     @RequestMapping(value="multifileUpload",method=RequestMethod.POST)
     /**public @ResponseBody String multifileUpload(@RequestParam("fileName")List<MultipartFile> files) */
-    public @ResponseBody String multifileUpload(HttpServletRequest request){
-
+    public @ResponseBody ServiceResult<String> multifileUpload(HttpServletRequest request){
+        ServiceResult<String> result=new ServiceResult<>();
         List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("fileName");
-
         if(files.isEmpty()){
-            return "false";
+            result.setSuccess(false);
+            result.setMessage("文件不能为空");
+            return result;
         }
 
         String path = "C:/picFile" ;
 
         for(MultipartFile file:files){
+
             String fileName = file.getOriginalFilename();
             int size = (int) file.getSize();
             System.out.println(fileName + "-->" + size);
 
             if(file.isEmpty()){
-                return "false";
+                result.setSuccess(false);
+                result.setMessage("文件不能为空");
             }else{
+                // 检查上传的图片的大小和扩展名
+                if (!checkFileForSpringUpload(file)) {
+                    result.setSuccess(false);
+                    result.setMessage("上传" + file.getOriginalFilename() + "的图片不符合限制");
+                    return result;
+//                throw new RuntimeException("上传" + file.getOriginalFilename() + "的图片不符合限制");
+                }
                 File dest = new File(path + "/" + fileName);
                 if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
                     dest.getParentFile().mkdir();
@@ -59,11 +84,12 @@ public class FileUploadController {
                     file.transferTo(dest);
                 }catch (Exception e) {
                     e.printStackTrace();
-                    return "false";
+                    result.setSuccess(true);
+                    result.setMessage("服务器出错了");
                 }
             }
         }
-        return "true";
+        return result;
     }
 
     /**
@@ -71,36 +97,65 @@ public class FileUploadController {
      * */
     @RequestMapping("fileUpload")
     @ResponseBody
-    public String fileUpload(@RequestParam("fileName") MultipartFile file){
-        if(file.isEmpty()){
-            return "false";
+    public ServiceResult<String> fileUpload(@RequestParam("fileName") MultipartFile file){
+        ServiceResult<String> result=new ServiceResult<>();
+        if (!file.isEmpty() && file.getSize() != 0) {
+            // 检查上传的图片的大小和扩展名
+            if (!checkFileForSpringUpload(file)) {
+                result.setSuccess(false);
+                result.setMessage("上传" + file.getOriginalFilename() + "的图片不符合限制");
+                return result;
+//                throw new RuntimeException("上传" + file.getOriginalFilename() + "的图片不符合限制");
+            }
+            String fileName = file.getOriginalFilename();
+            int size = (int) file.getSize();
+            System.out.println(fileName + "-->" + size);
+            String path = "C:/picFile" ;
+            File dest = new File(path + "/" + fileName);
+            if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
+                dest.getParentFile().mkdir();
+            }
+            try {
+                file.transferTo(dest); //保存文件
+                result.setSuccess(true);
+                result.setMessage("文件上传成功");
+            } catch (Exception e) {
+                e.printStackTrace();
+                result.setSuccess(false);
+                result.setMessage("服务器出错了");
+            }
+        } else {
+            result.setSuccess(false);
+            result.setMessage("文件不能为空");
         }
-        String fileName = file.getOriginalFilename();
-        int size = (int) file.getSize();
-        System.out.println(fileName + "-->" + size);
-
-        String path = "C:/picFile" ;
-        File dest = new File(path + "/" + fileName);
-        if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
-            dest.getParentFile().mkdir();
+        return result;
+    }
+/**
+ *@Author:zhangzijuan
+ *@date 2018/3/4
+ *@param:muFile
+ */
+    private static boolean checkFileForSpringUpload(MultipartFile muFile) {
+        boolean bool = true;
+        // 检查文件大小
+        if (muFile.getSize() > maxSize) {
+            logger.error("=============>上传" + muFile.getOriginalFilename() + "文件大小超过限制");
+            bool = false;
         }
-        try {
-            file.transferTo(dest); //保存文件
-            return "true";
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-            return "false";
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "false";
+        String fileName = muFile.getOriginalFilename();
+        // 检查扩展名
+        String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+        if (!Arrays.<String> asList(extMap.get(IMAGE).split(",")).contains(fileExt)) {
+            logger.error("上传文件" + muFile.getOriginalFilename() + "扩展名是不允许的扩展名。\n只允许"
+                    + extMap.get(IMAGE) + "格式。");
+            bool = false;
         }
+        return bool;
     }
 
-    /*
-     * 获取multifile.html页面
-     */
-    @RequestMapping("multifile")
-    public String multifile(){
-        return "/multifile";
+    // 初始化
+    static {
+        extMap.put(IMAGE, "gif,jpeg,png,bmp,ico");
     }
+
 }
