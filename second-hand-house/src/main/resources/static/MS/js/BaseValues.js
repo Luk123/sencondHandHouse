cf7e03abb2ae1e456ebc086029a35ce6c68acbe9
Luk123 <13619585427@163.com>
2018-03-16 22:35:31 +0800

    字典页的修改

--- second-hand-house/src/main/resources/static/MS/js/BaseValues.js
+++ second-hand-house/src/main/resources/static/MS/js/BaseValues.js
@@ -2,3 +2,10 @@
 /**basePath **/
 var basePath = "http://localhost:8088/";
 
+function formatDate(timestamp) {
+    var date = new Date(timestamp);
+    Y = date.getFullYear() + '-';
+    M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    D = date.getDate() < 10 ? '0'+(date.getDate()) : date.getDate();
+    return Y+M+D;
+}
