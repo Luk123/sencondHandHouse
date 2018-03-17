06ad51df2e38fc2339edf8945c0ad9a2013da4a4
maguangzu <949422060@qq.com>
2018-03-17 18:44:03 +0800

    add tag

--- second-hand-house/src/main/resources/static/MS/js/DateFormat.js
+++
@@ -1,7 +0,0 @@
-function formatDate(timestamp) {
-	var date = new Date(timestamp);
-	Y = date.getFullYear() + '-';
-	M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-	D = date.getDate() + ' ';
-	return Y+M+D;
-}
