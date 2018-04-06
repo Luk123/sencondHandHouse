d4bf1ee561f7bcbfa76d6f6217da577655b5de3c
Luk123 <13619585427@163.com>
2018-04-06 18:39:00 +0800

    代码调整

---
+++ second-hand-house/src/main/java/com/wintop/ms/config/MyWebAppConfiguration.java
@@ -0,0 +1,25 @@
+package com.wintop.ms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author zhangzijuan
+ * @Description:
+ * @date 2018-04-06
+ */
+@Configuration
+public class MyWebAppConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /**
+         * @Description: 对文件的路径进行配置,创建一个虚拟路径/Path/** ，即只要在<img src="/Path/picName.jpg" />便可以直接引用图片
+         *这是图片的物理路径  "file:/+本地图片的地址"
+         * @Date： Create in 14:08 2017/12/20
+         */
+        registry.addResourceHandler("/Path/**").addResourceLocations("file:/C:/picFile/");
+        super.addResourceHandlers(registry);
+    }
+}
--- second-hand-house/src/main/resources/static/usedhouse/js/common.js
+++ second-hand-house/src/main/resources/static/usedhouse/js/common.js
@@ -1,8 +1,8 @@
 /**
  * Created by tingguo on 2018/3/11.
  */
-$('footer').load("foot.html");
-$('header').load("head.html");
+$('footer').load("usedhouse/foot.html");
+$('header').load("usedhouse/head.html");
 
 window.userid=getCookie('key');
 window.username=getCookie('username');
